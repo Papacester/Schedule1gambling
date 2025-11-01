89e70b85db4ece90e9ecad50750228744489b0ac
Papacester <106293907+Papacester@users.noreply.github.com>
2025-10-31 20:24:50 -0600

    Update slotsFASTER.py
    took out comments and unnecessary lines

--- slotsFASTER.py
+++ slotsFASTER.py
@@ -1,17 +1,8 @@
 import keyboard
 import win32api
 import win32con
-import pyautogui
-import pynput
-from pynput.mouse import Button, Controller
 
 import time
-
-mouse = Controller()
-
-def leftClick():
-    time.sleep(0.1)
-    mouse.click(Button.left, 1)
 
 def Press(key):
     keyboard.press(key)
@@ -21,19 +12,15 @@ def Press(key):
 def moveMouse(x,y):
     if x >= 0:
         for i in range(0,x):
-            #time.sleep(0.0005)
             win32api.mouse_event(win32con.MOUSEEVENTF_MOVE, 1, 0, 0, 0)
     else:
         for i in range(x,0):
-            #time.sleep(0.0005)
             win32api.mouse_event(win32con.MOUSEEVENTF_MOVE, -1, 0, 0, 0)
     if y >= 0:
         for i in range(0,y):
-            #time.sleep(0.0005)
             win32api.mouse_event(win32con.MOUSEEVENTF_MOVE, 0, 1, 0, 0)
     else:
         for i in range(y,0):
-            #time.sleep(0.0005)
             win32api.mouse_event(win32con.MOUSEEVENTF_MOVE, 0, -1, 0, 0)
  
 time.sleep(3)
@@ -42,31 +29,16 @@ while True:
     Press("e")
     moveMouse(800,0)
     moveMouse(0,50)
-    #time.sleep(0.5)
     Press("e")
     moveMouse(750,0)
     moveMouse(0,-50)
-    #time.sleep(0.5)
     Press("e")
     moveMouse(870,0)
     moveMouse(0,50)
-    #time.sleep(0.5)
     Press("e")
     moveMouse(825,0)
     moveMouse(0,-50)
     Press("e")
-    #moveMouse(550,0)
-    #Press("e")
-    #moveMouse(-100,0)
-    #moveMouse(0,325)
-    #time.sleep(1)
-    #Press("e")
-    #time.sleep(1)
-    #leftClick()
-    #time.sleep(1)
-    #moveMouse(0,-325)
-    #moveMouse(100,0)
-    #moveMouse(-550,0)
     Press("e")
     moveMouse(0,50)
     moveMouse(-825,0)
@@ -80,4 +52,5 @@ while True:
     moveMouse(0,-50)
     moveMouse(-800,0)
     Press("e")
+
 
