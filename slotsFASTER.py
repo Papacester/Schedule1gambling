14912fb4ca30f456e559919a991ea5085c0c7e90
Papacester <106293907+Papacester@users.noreply.github.com>
2025-10-31 21:12:45 -0600

    Update slotsFASTER.py
    made an exit function

--- slotsFASTER.py
+++ slotsFASTER.py
@@ -1,26 +1,39 @@
+import sys
 import keyboard
 import win32api
 import win32con
 
 import time
 
+def shouldIquit():
+    if keyboard.is_pressed('esc'):
+        sys.exit()
+
 def Press(key):
+    shouldIquit()
     keyboard.press(key)
+    shouldIquit()
     time.sleep(0.1)
     keyboard.release(key)
+    shouldIquit()
 
 def moveMouse(x,y):
+    shouldIquit()
     if x >= 0:
         for i in range(0,x):
+            shouldIquit()
             win32api.mouse_event(win32con.MOUSEEVENTF_MOVE, 1, 0, 0, 0)
     else:
         for i in range(x,0):
+            shouldIquit()
             win32api.mouse_event(win32con.MOUSEEVENTF_MOVE, -1, 0, 0, 0)
     if y >= 0:
         for i in range(0,y):
+            shouldIquit()
             win32api.mouse_event(win32con.MOUSEEVENTF_MOVE, 0, 1, 0, 0)
     else:
         for i in range(y,0):
+            shouldIquit()
             win32api.mouse_event(win32con.MOUSEEVENTF_MOVE, 0, -1, 0, 0)
  
 time.sleep(3)
@@ -52,5 +65,4 @@ while True:
     moveMouse(0,-50)
     moveMouse(-800,0)
     Press("e")
-
 
